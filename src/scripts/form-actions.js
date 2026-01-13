4d1a847ddeae9ca16742311e12bb564d4184ba4a
Mauro-Domingues <maurinho.villa@hotmail.com>
2026-01-13 13:10:23 -0300

    copyjson-removed

--- src/scripts/form-actions.js
+++ src/scripts/form-actions.js
@@ -2,20 +2,19 @@ import { DataCollector } from './data-collector.js';
 import { RegisterTemplate } from './registerTemplate.js';
 
 export class FormActions {
-  registerTemplate
-  PDFService
+  #registerTemplate
 
   constructor() {
-    this.registerTemplate = new RegisterTemplate();
+    this.#registerTemplate = new RegisterTemplate();
   }
 
-  async updatePreview() {
+  async #updatePreview() {
     const variables = DataCollector.collect();
 
     const previewFrame = document.querySelector('#previewFrame');
 
     if (previewFrame) {
-      const htmlContent = await this.registerTemplate.getContent(variables);
+      const htmlContent = await this.#registerTemplate.getContent(variables);
 
       const blob = new Blob([htmlContent], { type: 'text/html;charset=utf-8' });
       const url = URL.createObjectURL(blob);
@@ -23,27 +22,27 @@ export class FormActions {
     }
   }
 
-  async setupAutoPreview() {
+  async #setupAutoPreview() {
     let debounceTimer;
     const updatePreviewDebounced = () => {
       clearTimeout(debounceTimer);
-      debounceTimer = setTimeout(async () => await this.updatePreview(), 500);
+      debounceTimer = setTimeout(async () => await this.#updatePreview(), 500);
     };
 
     const main = document.querySelector('main');
     main.addEventListener('input', updatePreviewDebounced);
     main.addEventListener('change', updatePreviewDebounced);
 
-    await this.updatePreview();
+    await this.#updatePreview();
   }
 
-  async setupDownloadPDFButton() {
+  async #setupDownloadPDFButton() {
     const previewBtn = document.querySelector('#downloadPdf');
     if (!previewBtn) return;
 
     previewBtn.addEventListener('click', async () => {
       const variables = DataCollector.collect();
-      const htmlContent = await this.registerTemplate.getContent(variables);
+      const htmlContent = await this.#registerTemplate.getContent(variables);
 
       const printWindow = window.open('', '_blank');
       printWindow.document.write(htmlContent);
@@ -57,21 +56,8 @@ export class FormActions {
     });
   }
 
-
-  setupCopyJsonButton() {
-    const copyJsonBtn = document.querySelector('#copyJson');
-    if (!copyJsonBtn) return;
-
-    copyJsonBtn.addEventListener('click', async () => {
-      const variables = DataCollector.collect();
-      const jsonString = JSON.stringify(variables, null, 2);
-      await navigator.clipboard.writeText(jsonString);
-    });
-  }
-
   setupAllActions() {
-    this.setupAutoPreview();
-    this.setupDownloadPDFButton();
-    this.setupCopyJsonButton();
+    this.#setupAutoPreview();
+    this.#setupDownloadPDFButton();
   }
 }
